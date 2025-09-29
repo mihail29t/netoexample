a0454e906fbe346d194853ea7933b65261379ab3
mihail29t <45531561+mihail29t@users.noreply.github.com>
2025-09-30 01:31:13 +0700

    Add files via upload

---
+++ task02.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int income = 50000;
+        int spending = 25000;
+        System.out.println("Итого (руб):");
+        System.out.println(income - spending);
+    }
+}
